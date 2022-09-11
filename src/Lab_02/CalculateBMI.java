14a4cd381fef692538ab1a78eab791d52ed8de04
BaoTramLT <tinalee0402@gmail.com>
2022-09-11 19:34:09 +0700

    Init

--- src/Lab_02/CalculateBMI.java
+++ src/Lab_02/CalculateBMI.java
@@ -1,2 +1,35 @@
-package Lab_02;public class CalculateBMI {
+package Lab_02;
+
+import java.util.Scanner;
+
+public class CalculateBMI {
+
+    public static void main(String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your weight and height: ");
+
+        double weight = scanner.nextDouble();
+        double height = scanner.nextDouble();
+        double BMI = weight/(height*2);
+
+        if (BMI < 18.5) {
+            System.out.println("Your are Underweight");
+            System.out.println("Please try to eat more to gain a normal weight");
+        }
+
+        else if (BMI <= 24.9) {
+            System.out.println("Congrats! You have a normal weight");
+        }
+
+        else if(BMI <= 29.9) {
+           System.out.println("Your are Overweight");
+           System.out.println("Please try to workout more to get a normal weight");
+        }
+
+        else {
+            System.out.println("You are Obesity");
+            System.out.println("Please try to workout more to get a normal weight");
+        }
+    }
 }
--- src/Lab_02/OddOrNumber.java
+++ src/Lab_02/OddOrNumber.java
@@ -1,2 +1,17 @@
-package Lab_02;public class OddOrNumber {
+package Lab_02;
+
+import java.util.Scanner;
+
+public class OddOrNumber {
+    public static void main(String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please input a number: ");
+        int number = scanner.nextInt();
+
+        if (number%2==0) System.out.println("La so chan");
+        else System.out.println("La so le");
+
+    }
 }
