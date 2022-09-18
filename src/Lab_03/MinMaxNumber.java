13d2fd3074780ae7b4eb8614123000f9bb483c08
BaoTramLT <tinalee0402@gmail.com>
2022-09-18 20:38:21 +0700

    Lab 03

--- src/Lab_03/MinMaxNumber.java
+++ src/Lab_03/MinMaxNumber.java
@@ -1,2 +1,39 @@
-package Lab_03;public class MinMaxNumber {
+package Lab_03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinMaxNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input array length: ");
+        int arrLength = scanner.nextInt();
+
+        int[] arrNumber = new int[arrLength];
+        int minNumber = arrNumber[0];
+        int maxNumber = arrNumber[0];
+        for (int i = 0; i < arrLength; i++) {
+            arrNumber[i] = i;
+        }
+        System.out.println("Array number is: ");
+        System.out.println(Arrays.toString(arrNumber));
+        System.out.println("\n");
+
+        for (int i = 0; i < arrLength; i++) {
+            arrNumber[i] = i;
+            {
+                if(arrNumber[i] < minNumber) {
+                minNumber = arrNumber[i];
+
+                }else if (arrNumber[i] > maxNumber) {
+                maxNumber = arrNumber[i];
+                }
+            }
+        }
+        System.out.printf("Min number is: %d", minNumber);
+        System.out.printf("\nMax number is: %d", maxNumber);
+
+    }
 }
---
+++ src/Lab_03/SortArray.java
@@ -0,0 +1,37 @@
+package Lab_03;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+
+    public static void main(String[] args) {
+
+        Scanner scannerArrLength = new Scanner(System.in);
+        System.out.println("Please input an array length: ");
+        int arrLength = scannerArrLength.nextInt();
+        int[] arrNumber = new int[arrLength];
+        int temp;
+        for (int i = 0; i < arrLength; i++) {
+            int randomNumber = new SecureRandom().nextInt(100);
+            arrNumber[i] = randomNumber;
+        }
+        System.out.println("\nYour random number is: ");
+        System.out.println(Arrays.toString(arrNumber));
+        System.out.println("\n");
+
+        for (int i = 0; i < (arrLength - 1); i++) {
+            for (int j = 0; j < arrLength - i - 1; j++) {
+                if (arrNumber[j] > arrNumber[j+1]) {
+                    temp = arrNumber[j];
+                    arrNumber[j] = arrNumber[j+1];
+                    arrNumber[j+1] = temp;
+                }
+             }
+        }
+        System.out.println("The sorted array is: ");
+        for (int i = 0; i < arrLength; i++);
+        System.out.println(Arrays.toString(arrNumber));
+    }
+}
